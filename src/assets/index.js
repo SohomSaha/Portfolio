621ea92d1b5421da34b8932b8d69397c4c563dfb
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2024-12-20 09:18:29 +0530

    contacts left only

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import Unified_Mentor from "./company/Unified_Mentor.ico"
+import sih from "./company/sih.ico"
+import girlcode from "./company/girlcode.ico"
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  Unified_Mentor,
+  sih,
+  girlcode,
   carrent,
   jobit,
   tripguide,
